020b0655dc42f6543cc7815334a327c35d2839eb
Danil <ugarovd06@gmail.com>
2024-05-13 22:34:24 +0300

    Added method for creating a table

--- PythonPro/bark_project/manager_db.py
+++ PythonPro/bark_project/manager_db.py
@@ -1,6 +1,14 @@
 from sqlite3 import connect
 
 
+template_create_table_query = """
+CREATE TABLE IF NOT EXISTS {table_name}
+(
+    {table_columns}
+);
+"""
+
+
 class DatabaseManager:
     connection = None
 
@@ -12,6 +20,13 @@ class DatabaseManager:
             cursor = self.connection.cursor()
             cursor.execute(statement, values or [])
             return cursor
+
+    def create_table(self, table_name, columns):
+        columns_with_types = [f"{column_name} {data_type}" for column_name, data_type in columns.items()]
+        string_columns = ",\n".join(columns_with_types)
+        create_table_query = template_create_table_query.format(table_name=table_name,
+                                                                table_columns=string_columns)
+        self._execute(create_table_query)
 
     def __del__(self):
         self.connection.close()
